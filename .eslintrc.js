62552aeaa6f11866d2771abf0adb80197bc009f8
Stefano Acerbetti <acerbetti@gmail.com>
2025-09-24 12:00:26 -0700

    Add root ESLint configuration for monorepo
    - Create .eslintrc.js at root level for shared linting config
    - Install ESLint dependencies (eslint, @typescript-eslint/eslint-plugin, @typescript-eslint/parser)
    - Fix 'ESLint couldn't find the config' error in CI
    - Use compatible versions with existing package configurations

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    // Allow console statements in development
+    'no-console': 'off',
+    // Allow unused variables that start with underscore
+    'no-unused-vars': 'off', // Let TypeScript handle this
+    // Allow require statements
+    'no-undef': 'off', // TypeScript handles this
+  },
+  env: {
+    node: true,
+    es2020: true,
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'coverage/',
+    '*.js',
+    '*.d.ts',
+  ],
+};
